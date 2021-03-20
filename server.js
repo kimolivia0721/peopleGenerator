9f561ffd38a94f088e805d81cb13af92d98f7fc1
Olivia Kim <kimolivia0721@gmail.com>
2021-03-19 22:37:26 -0400

    move files

--- server.js
+++ server.js
@@ -28,6 +28,8 @@ app.get('/', (req, res) => {
 	//sending some HTML
 	res.send('<h1>This should be the root route!</h1>')
 	res.sendFile('/pub/examples.js')
+	res.sendFile('/pub/library.js')
+	res.sendFile('/pub/examples.html')
 })
 
 // Error codes
