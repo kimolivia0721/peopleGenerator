80f300a2615e65e0eff8ac4b8a7797e7abc358d2
Olivia Kim <kimolivia0721@gmail.com>
2021-03-19 23:23:11 -0400

    move files

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const path = require('path');
 // Setting up a static directory for the files in /pub
 // using Express middleware.
 // Don't put anything in /pub that you don't want the public to have access to!
-app.use(express.static(__dirname + "/../pub"))
+// app.use(express.static(__dirname + "/../pub"))
 // Let's make some express 'routes'
 // Express has something called a Router, which 
 // takes specific HTTP requests and handles them
